5d2c14332ace0c4e7ff44ebec0def106faee1539
blakedietz <blakedietz@gmail.com>
2020-07-12 18:06:39 -0600

    feat(blog-posts.js): Add tags component to links

--- src/pages/blog-posts.js
+++ src/pages/blog-posts.js
@@ -1,8 +1,28 @@
 import React from "react"
 import { graphql } from "gatsby"
-import PostLink from "../components/post-link"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import { Link } from "gatsby"
+import { getFormattedDate } from "../utils/utils"
+import "./blog-posts.css"
+import {Pills} from "../components/pill"
+
+const PostLink = ({ post }) => (
+  <div className="post-and-description-container">
+    <Link className="post-title text-xl my-6" to={post.frontmatter.path}>
+        {post.frontmatter.title}
+    </Link>
+    <div className="post-description">
+      {post.frontmatter.description}
+    </div>
+    <div className="post-tags">
+      {post.frontmatter.tags && (<Pills pills={post.frontmatter.tags}/>)}
+    </div>
+    <div className="post-date">
+      {getFormattedDate((new Date(post.frontmatter.date)))}
+    </div>
+  </div>
+)
 
 const BlogPostsPage = ({
                          data: {
@@ -33,6 +53,8 @@ export const pageQuery = graphql`
                     excerpt(pruneLength: 250)
                     frontmatter {
                         date(formatString: "MMMM DD, YYYY")
+                        description
+                        tags
                         path
                         title
                     }
