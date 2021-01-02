51d7d48c04618422831564681c7110b5c8e01e3b
blakedietz <blakedietz@gmail.com>
2021-01-02 15:17:34 -0700

    Change profile photo

--- src/components/image.js
+++ src/components/image.js
@@ -16,7 +16,7 @@ import Img from "gatsby-image"
 const Image = ({ imagePath }) => {
   const data = useStaticQuery(graphql`
     query {
-      placeholderImage: file(relativePath: { eq: "quinns-pond-profile.png" }) {
+      placeholderImage: file(relativePath: { eq: "parking-profile.JPG" }) {
         childImageSharp {
           fluid(maxWidth: 300) {
             ...GatsbyImageSharpFluid
