0bb3f446fc1560133316d1c59c5114973370f1f9
blakedietz <blakedietz@gmail.com>
2021-01-02 15:07:56 -0700

    Add some links that discuss di in elixir

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,17 +7,17 @@ const AboutPage = () => (
   <Layout>
     <SEO title="About" />
     <h1>About</h1>
-    <div style={{ maxWidth: `500px`, marginBottom: `1.45rem` }}>
-      <Image />
+    <div style={{display: "flex", justifyContent: "center"}}>
+      <div style={{ width: "100%", display: "grid", gridGap: "25px", gridTemplateColumns: "repeat(2, minmax(0, 1fr))", marginBottom: `1.45rem` }}>
+        <p>
+          I'm a human who codes (sometimes known as a software engineer). I'm located in Boise, ID. Allyanna Wintermote
+          is my <a href={"https://instagram.com/allyan_na"}> partner</a>. Velo is my{" "}
+          <a href={"https://instagram.com/velo.dog"}>pupper</a>. I'm a lifelong learner. My hobbies include vintage pen
+          restoration, keyboard building, mountain biking, kombucha brewing, lifting and rock climbing.
+        </p>
+        <Image />
+      </div>
     </div>
-    <p>
-      I'm a software developer that lives in Boise, ID. I have a
-      <a href={"https://instagram.com/allyan_na"}> partner</a> and a{" "}
-      <a href={"https://instagram.com/velo.dog"}>pupper</a> that I spend my
-      spare time with. I'm a life long learner, that spends way too much time
-      tinkering. My hobbies include vintage pen restoration, keyboard building,
-      mountain biking, kombucha brewing, lifting and rock climbing.
-    </p>
   </Layout>
 )
 
