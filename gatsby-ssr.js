f524c4d2eee295e957512d1658760be66d132a2d
blakedietz <blakedietz@gmail.com>
2022-01-22 14:09:49 -0700

    fix: missing id for react

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -8,12 +8,13 @@
 import React from "react"
 export function onRenderBody({ setHeadComponents }) {
   setHeadComponents(
-    <>
+    [
       <script
+        id="plausible"
         defer
         data-domain="blakedietz.me"
         src="https://plausible.blakedietz.me/js/plausible.js"
       ></script>
-    </>
+  ]
   )
 }
